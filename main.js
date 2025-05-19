51dd29ffb9cd89b3af9e7e9937200e5cfc9d8c41
pruje839 <jeffprumbaum@hotmail.com>
2025-05-19 16:38:43 +0200

    Hausaufgabe 8

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ let map = L.map("map").setView([ibk.lat, ibk.lng], 5);
 // thematische Layer
 let overlays = {
     forecast: L.featureGroup().addTo(map),
-    wind: L.featureGroup().addTo(map)
+    wind: L.featureGroup().addTo(map),
+    winddirection: L.featureGroup().addTo(map),
 }
 
 // Layer Control
@@ -23,6 +24,7 @@ let layerControl = L.control.layers({
 }, {
     "Wettervorhersage MET Norway": overlays.forecast,
     "ECMWF Windvorhersage": overlays.wind,
+    "ECMWF Windrichtung": overlays.winddirection,
 }).addTo(map);
 
 // Maßstab
@@ -99,3 +101,25 @@ map.fire("click", {
         lng: ibk.lng,
     }
 })
+
+// Velocity Layer
+fetch('https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json')
+    .then(res => res.json())
+    .then(data => {
+        const velocityLayer = L.velocityLayer({
+            data: data,
+            displayValues: true,
+            displayOptions: {
+                velocityType: "Global Wind",
+                position: "bottomleft",
+                emptyString: "No velocity data",
+                angleConvention: "bearingCW",
+                showCardinal: false,
+                speedUnit: "ms",
+                directionString: "Direction",
+                speedString: "Speed",
+            },
+        maxVelocity: 10,
+        });
+        velocityLayer.addTo(overlays.winddirection)
+    });
